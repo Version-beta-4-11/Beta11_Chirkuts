97118a0b47632215eadbef07233c264b28c81e51
mansidas19 <mansidas19@rediffmail.com>
2021-10-30 11:30:42 +0530

    weather page added

--- app/kisan_seva/lib/screens/home.dart
+++ app/kisan_seva/lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'weather.dart';
 
 class Home extends StatefulWidget {
   static final String routeName = 'home';
@@ -20,7 +21,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [],title: Text("kisan sewa"),backgroundColor: Color(0xFF50fa78),),
+      appBar: AppBar(
+        title: Text("kisan sewa"),
+        backgroundColor: Color(0xFF50fa78),
+      ),
     );
   }
 }
